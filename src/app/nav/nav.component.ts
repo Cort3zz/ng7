711865e024ad831fb2c933fe5a4e17ed9a04edcf
Nagy Norbert <nnorbian@gmail.com>
2019-05-24 17:02:48 +0200

    initial

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+declare let $: any;
+
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
